1217a6233aadbb1d3f5f448aa8b5d520ce33dd57
pledgedavis <pledgedavis14@gmail.com>
2019-11-25 07:23:20 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,34 @@
-def square_array(array)
-  # your code here
-end
+# def square_array(array)
+#   # your code here
+  
+#   numbers = [1,4,3]
+  
+# square_array(numbers) 
+  
+#   new_numbers = [1,4,8,6]
+  
+# return  square_array(new_numbers)
+# end
+# end
+
+
+
+
+
+ def square_array(array)
+    # your code here
+ new_array = []
+   counter = 0
+   while counter < array.length do 
+    new_array << (array[counter])**2
+    counter += 1 
+   end
+   return new_array
+ end
+
+
+
+
+
+
+
